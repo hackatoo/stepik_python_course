383426039ad4539f537be756901d90022a2474c8
Hackatoo <64157286+hackatoo@users.noreply.github.com>
2022-02-16 12:57:53 +0300

    Create ifelse_smallest_from_4.py

---
+++ ifelse_smallest_from_4.py
@@ -0,0 +1,11 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+min=0
+if a > min: 
+    min+=a
+if b < min:
+    min=b
+if c < min: 
+    min=c
+if d < min: 
+    min=d
+print(min)
