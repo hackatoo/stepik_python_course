38d2e02c3b5b59a9931b0bec49977ef3e7117ee6
Hackatoo <64157286+hackatoo@users.noreply.github.com>
2022-02-16 13:34:25 +0300

    Create final_test_roman_nums.py

---
+++ final_test_roman_nums.py
@@ -0,0 +1,35 @@
+num=int(input())
+one=1
+two=2
+three=3
+four=4
+five=5
+six=6
+seven=7
+eight=8
+nine=9
+ten=10
+
+
+if num == one:
+    print("I")
+elif num == two:
+    print("II")
+elif num == three:
+    print("III")
+elif num == four:
+    print("IV")
+elif num == five:
+    print("V")
+elif num == six:
+    print("VI")
+elif num == seven:
+    print("VII")
+elif num == eight:
+    print("VIII")
+elif num == nine:
+    print("IX")
+elif num == ten:
+    print("X")
+else:
+    print("ошибка")
